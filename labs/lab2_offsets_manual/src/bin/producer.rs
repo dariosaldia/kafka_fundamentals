c8495ddd3dd39c2cb8d88ddbb65d0ba43d9a22dc
dariosaldia <dariosaldia@gmail.com>
2025-08-16 14:50:26 +0200

    refactor(labs/lab2): use record creation function from shared

--- labs/lab2_offsets_manual/src/bin/producer.rs
+++ labs/lab2_offsets_manual/src/bin/producer.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
-use rdkafka::producer::FutureRecord;
 use rdkafka::producer::future_producer::Delivery;
-use shared::config::{AppConfig, PartitioningMode};
+use shared::config::AppConfig;
 use shared::create_producer_props;
 use shared::event::Event;
+use shared::record::create_future_record;
 use std::env;
 use std::io::{self, BufRead};
 use std::time::Duration;
@@ -12,7 +12,7 @@ use std::time::Duration;
 async fn main() -> Result<()> {
     let mut args = env::args();
     let mut cfg_path = "shared/config.toml".to_string();
-    let mut profile = "lab2.keyed".to_string();
+    let mut profile = "lab2.default".to_string();
 
     while let Some(a) = args.next() {
         match a.as_str() {
@@ -64,14 +64,8 @@ async fn main() -> Result<()> {
 
         let payload = serde_json::to_vec(&evt)?;
 
-        let record = match cfg.partitioning {
-            PartitioningMode::Keyed => FutureRecord::to(&cfg.topic)
-                .key(&evt.user_id)
-                .payload(&payload),
-            PartitioningMode::RoundRobin => {
-                FutureRecord::to(&cfg.topic).payload(&payload) // NO KEY
-            }
-        };
+        let record =
+            create_future_record(Some(&evt.user_id), &payload, &cfg.topic, cfg.partitioning)?;
 
         let delivery = producer.send(record, Duration::from_secs(0)).await;
 
